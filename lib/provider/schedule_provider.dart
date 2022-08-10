3c4debf5d64a539e89115fb78f1c9d3d4067c6e3
Bagas Makhali <makhalibagas1@gmail.com>
2022-08-10 08:39:57 +0700

    sub 3

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/widgets.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
-import 'package:restaurant_flutter/data/database/db_service.dart';
 import 'package:restaurant_flutter/utils/background_service.dart';
 import 'package:restaurant_flutter/utils/date_time_helper.dart';
 
 class SettingsProvider extends ChangeNotifier {
-  DatabaseService databaseService = DatabaseService();
   bool _isScheduled = false;
 
   bool get isScheduled => _isScheduled;
 
   Future<bool> scheduledInfo(bool b) async {
     _isScheduled = b;
+    debugPrint('b adalah $b');
     if (_isScheduled) {
       debugPrint('Scheduling info Activates');
       notifyListeners();
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:restaurant_flutter/main.dart';
+import 'package:restaurant_flutter/data/models/restaurant.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  var data = {
+    "id": "rqdv5juczeskfw1e867",
+    "name": "Melting Pot",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ...",
+    "pictureId": "14",
+    "city": "Medan",
+    "rating": 4.2
+  };
+  test("Tes Json Parsing", () async {
+    var result = Restaurant.fromJson(data).id;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(result, "rqdv5juczeskfw1e867");
   });
 }
